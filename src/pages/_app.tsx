67e8bb05876dcd608ba79677e65ed70eecf33fb5
Vasin Sermsampan <dev.vasin@gmail.com>
2024-04-05 14:46:17 +0700

    chore(seo): move meta tag from document to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { RainbowKitProvider, Theme, darkTheme } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import { merge } from 'lodash'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ReactElement, ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { ThemeProvider, createGlobalStyle, css, keyframes } from 'styled-components'
@@ -161,32 +163,76 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider theme={rainbowKitTheme} chains={chains}>
-          <TransactionStoreProvider>
-            <EnsProvider>
-              <ThemeProvider theme={thorinGlobalTheme}>
-                <BreakpointProvider queries={breakpoints}>
-                  <GlobalStyle />
-                  <ThorinGlobalStyles theme={thorinGlobalTheme} />
-                  <GlobalErrorProvider>
-                    <SyncProvider>
-                      <TransactionFlowProvider>
-                        <SyncDroppedTransaction>
-                          <Notifications />
-                          <Basic>{getLayout(<Component {...pageProps} />)}</Basic>
-                        </SyncDroppedTransaction>
-                      </TransactionFlowProvider>
-                    </SyncProvider>
-                  </GlobalErrorProvider>
-                </BreakpointProvider>
-              </ThemeProvider>
-            </EnsProvider>
-          </TransactionStoreProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </I18nextProvider>
+    <>
+      <Head>
+        <title>JNS</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="title" content="JNS" />
+        <meta
+          name="description"
+          content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jns-app.pages.dev/" />
+        <meta property="og:title" content="JNS" />
+        <meta
+          property="og:description"
+          content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
+        />
+        <meta property="og:image" content="/cover.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://jns-app.pages.dev/" />
+        <meta property="twitter:title" content="JNS" />
+        <meta
+          property="twitter:description"
+          content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
+        />
+        <meta property="twitter:image" content="/cover.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <I18nextProvider i18n={i18n}>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider theme={rainbowKitTheme} chains={chains}>
+            <TransactionStoreProvider>
+              <EnsProvider>
+                <ThemeProvider theme={thorinGlobalTheme}>
+                  <BreakpointProvider queries={breakpoints}>
+                    <GlobalStyle />
+                    <ThorinGlobalStyles theme={thorinGlobalTheme} />
+                    <GlobalErrorProvider>
+                      <SyncProvider>
+                        <TransactionFlowProvider>
+                          <SyncDroppedTransaction>
+                            <Notifications />
+                            <Basic>{getLayout(<Component {...pageProps} />)}</Basic>
+                          </SyncDroppedTransaction>
+                        </TransactionFlowProvider>
+                      </SyncProvider>
+                    </GlobalErrorProvider>
+                  </BreakpointProvider>
+                </ThemeProvider>
+              </EnsProvider>
+            </TransactionStoreProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </I18nextProvider>
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-title-in-document-head */
 import { AppPropsType, AppType } from 'next/dist/shared/lib/utils'
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
@@ -75,53 +74,12 @@ export default class MyDocument extends Document {
               <link rel="stylesheet" href="./fonts/fonts.css" />
             </>
           )}
-          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#ffffff" />
+
           <script
             defer
             data-domain="app.ens.domains"
             src="https://plausible.io/js/script.outbound-links.js"
           />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <title>JNS</title>
-          <meta name="title" content="JNS" />
-          <meta
-            name="description"
-            content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
-          />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://jns-app.pages.dev/" />
-          <meta property="og:title" content="JNS" />
-          <meta
-            property="og:description"
-            content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
-          />
-          <meta property="og:image" content="/cover.png" />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://jns-app.pages.dev/" />
-          <meta property="twitter:title" content="JNS" />
-          <meta
-            property="twitter:description"
-            content="JNS is your unified gateway to the decentralized web. A singular name for all your blockchain addresses and your portal to a decentralized presence."
-          />
-          <meta property="twitter:image" content="/cover.png" />
         </Head>
         <body>
           <Main />
