a4a90d2a76826c519a44de8f86fd96041cffb492
Vasin Sermsampan <dev.vasin@gmail.com>
2024-04-22 09:58:13 +0700

    update

--- src/transaction-flow/input/SelectPrimaryName/SelectPrimaryName-flow.tsx
+++ src/transaction-flow/input/SelectPrimaryName/SelectPrimaryName-flow.tsx
@@ -349,7 +349,6 @@ const SelectPrimaryName = ({ data: { address }, dispatch, onDismiss }: Props) =>
           </>
         )}
         <StyledScrollBox hideDividers onReachedBottom={loadMoreNames}>
-          <TaggedNameItemWithFuseCheck key="fwef" name="fwef" network={0} />
           {!!namesData && namesData.pages[0].length > 0 ? (
             <>
               {namesData.pages?.map((page: Name[]) =>
